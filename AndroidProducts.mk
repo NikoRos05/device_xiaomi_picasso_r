9f10484a02bfc54a9e98688038c2aa791caa5a3b
NikoRos05 <sorok310@gmail.com>
2021-01-26 20:47:02 +0300

    picasso: test 2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 OctaviOS
+# Copyright (C) 2020 PixelExperience
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_picasso.mk
+    $(LOCAL_DIR)/aosp_picasso.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_picasso-eng \
-    octavi_picasso-userdebug \
-    octavi_picasso-user
+    aosp_picasso-eng \
+    aosp_picasso-userdebug \
+    aosp_picasso-user
--- aosp_picasso.mk
+++ aosp_picasso.mk
@@ -1,27 +1,34 @@
 #
-# Copyright (C) 2020 OctaviOS
+# Copyright (C) 2021 PixelExperience
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# ANXCamera
+$(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common OctaviOS stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := octavi_picasso
+PRODUCT_NAME := aosp_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
+PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi K30 5G
-PRODUCT_MANUFACTURER := Redmi
 
-# Boot animation
+# PixelExperience Properties
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# OctaviOS maintainter
-OCTAVI_DEVICE_MAINTAINER := masemoel
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Adds face unlock if package is available on ROM source.
 TARGET_SUPPORT_FACE_UNLOCK := true
@@ -34,6 +41,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=picasso \
     PRODUCT_NAME=picasso \
-    PRIVATE_BUILD_DISC="coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+    PRIVATE_BUILD_DISC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
